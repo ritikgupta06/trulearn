67b32577eb3cc312a70e8593800fa713378cd69c
Ritik Gupta <142598621+ritikgupta06@users.noreply.github.com>
2024-09-08 21:01:45 +0530

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      there i will call all the elements
+      there i will call all the element
     </>
   );
 }
